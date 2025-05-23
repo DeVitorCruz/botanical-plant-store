3551074faac4d7ed35d42751d82a1917dadb8b18
DeVitor <97829726+DeVitorCruz@users.noreply.github.com>
2025-05-22 21:34:50 -0300

    The component "justify" was created and inserted at the section of the component "main" with all this features.

--- src/app/home/components/main/components/justify/justify.component.ts
+++ src/app/home/components/main/components/justify/justify.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-justify',
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './justify.component.html',
   styleUrl: './justify.component.scss'
 })
 export class JustifyComponent {
-
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon('leaf-solid', 
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/justify-img/icons/leaf-solid.svg')
+    );
+    this.matIconRegistry.addSvgIcon('seedling-solid', 
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/justify-img/icons/seedling-solid.svg')
+    );
+    this.matIconRegistry.addSvgIcon('tree-solid', 
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/justify-img/icons/tree-solid.svg')
+    );
+  }
 }
